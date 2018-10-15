70cdb2d68fc1d952fc85727be4515b9ab2888450
kyumoon bae <bgm8547@gmail.com>
2018-10-15 23:36:46 +0900

    initial commit

---
+++ src/main/java/com/bowling/bae/BaeApplication.java
@@ -0,0 +1,12 @@
+package com.bowling.bae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaeApplication.class, args);
+    }
+}
