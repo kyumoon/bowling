e5fef605714eb342e2c5c150f2572c7f3efe688d
kyumoon bae <bgm8547@gmail.com>
2018-10-16 00:51:37 +0900

    static path setting

--- src/main/java/com/bowling/bae/BaeApplication.java
+++ src/main/java/com/bowling/bae/BaeApplication.java
@@ -2,6 +2,7 @@ package com.bowling.bae;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 public class BaeApplication {
